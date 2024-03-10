a7ec4850a9522e15cd359319e63c46eb6855c95a
dtdasilva <dtdasilva@rd.com.br>
2024-03-10 15:58:51 -0300

    Hello world with serverless framework

---
+++ 03-serverless-framework/run.sh
@@ -0,0 +1,19 @@
+#instalar
+npm i -g serverless
+
+#inicializar
+sls
+
+#template HTTP NODE API
+
+#comando de deploy para cada atualização
+sls deploy
+
+#traz as funções e endpoints
+sls info
+
+#chamar função local
+sls invoke local -f handler -log
+
+#remover
+sls remove
